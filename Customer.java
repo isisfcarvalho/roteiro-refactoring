7f33b00f42d3b7fceb53cba9069e66532439b96d
Isis <64763785+isisfcarvalho@users.noreply.github.com>
2022-11-28 17:15:08 +0000

    Commit 8

--- Customer.java
+++ Customer.java
@@ -34,24 +34,42 @@ public class Customer {
                      " frequent renter points";
       return result;
    }
-      
-   private double getTotalCharge() {
-      double result = 0;
-      Enumeration rentals = _rentals.elements();
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         result += each.getCharge();
-         }
-         return result;
-   }
 
-   private int getTotalFrequentRenterPoints(){
-      int result = 0;
+  public String htmlStatement() {
       Enumeration rentals = _rentals.elements();
+      String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
-         result += each.getFrequentRenterPoints();
+         // show figures for each rental
+         result += each.getMovie().getTitle()+ ": " +
+                  String.valueOf(each.getCharge()) + "<BR>\n";
       }
+      
+      // add footer lines
+      result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+      result += "On this rental you earned <EM>" +
+            String.valueOf(getTotalFrequentRenterPoints()) +
+            "</EM> frequent renter points<P>";
       return result;
    }
+      
+  private double getTotalCharge() {
+    double result = 0;
+    Enumeration rentals = _rentals.elements();
+    while (rentals.hasMoreElements()) {
+       Rental each = (Rental) rentals.nextElement();
+       result += each.getCharge();
+       }
+       return result;
+  }
+
+  private int getTotalFrequentRenterPoints(){
+    int result = 0;
+    Enumeration rentals = _rentals.elements();
+    while (rentals.hasMoreElements()) {
+       Rental each = (Rental) rentals.nextElement();
+       result += each.getFrequentRenterPoints();
+    }
+    return result;
+  }
 }
